093633d367134178716a357326b39bc4f3d4c54a
SKCrawford <s.kyle.crawford@gmail.com>
2019-07-12 20:18:26 -0700

    Chore: remove graphql and react-router dependencies. Version match babel-loader and eslint to react-scripts.

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,5 +1,4 @@
 import { addDecorator, configure } from '@storybook/react';
-import StoryRouter from 'storybook-react-router';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /\.stories\.tsx$/);
@@ -7,7 +6,5 @@ const req = require.context('../src', true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(req);
 }
-
-addDecorator(StoryRouter())
 
 configure(loadStories, module);
--- src/router.ts
+++
@@ -1,4 +0,0 @@
-import { createBrowserHistory } from 'history';
-
-/** The history object used by react-router. */
-export const history = createBrowserHistory();
