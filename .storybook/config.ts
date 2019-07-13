a9e776c9fe54464b57e6de720369eb90f9c80d8f
SKCrawford <s.kyle.crawford@gmail.com>
2019-07-12 20:21:43 -0700

    Add required node modules and hook them into storybook - Chore: add styled components dependencies - Chore: add websockets dependencies

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,4 +1,7 @@
 import { addDecorator, configure } from '@storybook/react';
+import { withThemes } from 'storybook-styled-components';
+
+import { theme } from '../src/theme';
 
 // automatically import all files ending in *.stories.tsx
 const req = require.context('../src', true, /\.stories\.tsx$/);
@@ -6,5 +9,11 @@ const req = require.context('../src', true, /\.stories\.tsx$/);
 function loadStories() {
   req.keys().forEach(req);
 }
+
+const themes = {
+  'default': theme,
+};
+
+addDecorator(withThemes(themes))
 
 configure(loadStories, module);
